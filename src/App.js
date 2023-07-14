83984b50b4c1dac410dd1ed9ceb113c853931ced
badermah <bmahdawi1@student.rccd.edu>
2023-07-14 16:15:57 -0700

    App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import myImage from "../images/me.png";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="image-container">
+        <img src={myImage} alt="About Me" />
+      </div>
+      <div className="text-container">
+        <h2>About Me</h2>
+        <p>
+          Bader is a driven and ambitious individual who has recently graduated
+          from the University of Berkeley's Full Stack Web Development Bootcamp.
+          With a strong passion for technology and an unwavering determination
+          to succeed, Bader is ready to embark on an exciting journey in the
+          world of web development.
+        </p>
+        <p>
+          During his time at the University of Berkeley, Bader honed his skills
+          in various aspects of web development, including front-end and
+          back-end technologies. He has a solid foundation in HTML, CSS, and
+          JavaScript, allowing him to create visually appealing and
+          user-friendly websites. Bader's comprehensive knowledge of web
+          development frameworks, such as React and Angular, enables him to
+          build dynamic and interactive web applications.
+        </p>
+        <p>
+          Bader's enthusiasm for learning and his ability to adapt to new
+          technologies sets him apart. He stays up-to-date with the latest
+          trends in web development and eagerly explores emerging frameworks and
+          tools. His curiosity and willingness to take on challenges make him an
+          asset in the ever-evolving field of technology.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
