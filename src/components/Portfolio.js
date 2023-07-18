99829748f3177367fcf336a7e5124cc66cd38d64
badermah <bmahdawi1@student.rccd.edu>
2023-07-17 19:00:56 -0700

    Resume.js and portfolio.js draft

---
+++ src/components/Portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const PortfolioHomePage = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        {}
+        <h1>Welcome to My Portfolio</h1>
+        {}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PortfolioHomePage;
---
+++ src/components/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>My Resume</h1>
+      <embed src="/assets/documents/myResume" type="application/pdf" width="100%" height="600px" />
+    </div>
+  );
+};
+
+export default Resume;
